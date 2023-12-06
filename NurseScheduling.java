6aa93e59ad558bd3576baaf58eea2df0c03a26d4
Chaitanya Patil <chaitanya.191423104@vcet.edu.in>
2023-12-06 17:02:20 +0530

    The Input Data - Creating an array for required model

--- NurseScheduling.java
+++ NurseScheduling.java
@@ -9,3 +9,19 @@ import com.google.ortools.sat.Literal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
+
+public class NurseScheduling {
+  public static void main(String[] args) {
+    Loader.loadNativeLibraries();
+final int numNurses = 5;
+final int numDays = 7;
+final int numShifts = 3;
+
+final int[] allNurses = IntStream.range(0, numNurses).toArray();
+final int[] allDays = IntStream.range(0, numDays).toArray();
+final int[] allShifts = IntStream.range(0, numShifts).toArray();
+
+
+
+  }
+}
