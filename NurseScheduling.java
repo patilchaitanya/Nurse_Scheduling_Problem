5250130b8e8c3c11911d5c3d450bba32cd2967f8
Chaitanya Patil <chaitanya.191423104@vcet.edu.in>
2023-12-06 17:15:18 +0530

    1.Creating the model and array of 'shifts' to have every combo of nurse, days and shifts 2. Added Shift request too in triples - for ex the triple [0, 0, 1] in the fifth position of row 1 indicates that nurse 1 requests shift 3 on day

--- NurseScheduling.java
+++ NurseScheduling.java
@@ -21,7 +21,66 @@ final int[] allNurses = IntStream.range(0, numNurses).toArray();
 final int[] allDays = IntStream.range(0, numDays).toArray();
 final int[] allShifts = IntStream.range(0, numShifts).toArray();
 
+// Set of triples, corresponding to the three shifts per day.
+// Each element of the triple is 0 or 1, indicating whether a shift was requested. For example, the triple [0, 0, 1] in the fifth position of row 1 indicates that nurse 1 requests shift 3 on day 5.
+final int[][][] shiftRequests = new int[][][] {
+    {
+        {0, 0, 1},
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 1},
+        {0, 1, 0},
+        {0, 0, 1},
+    },
+    {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 1, 0},
+        {0, 1, 0},
+        {1, 0, 0},
+        {0, 0, 0},
+        {0, 0, 1},
+    },
+    {
+        {0, 1, 0},
+        {0, 1, 0},
+        {0, 0, 0},
+        {1, 0, 0},
+        {0, 0, 0},
+        {0, 1, 0},
+        {0, 0, 0},
+    },
+    {
+        {0, 0, 1},
+        {0, 0, 0},
+        {1, 0, 0},
+        {0, 1, 0},
+        {0, 0, 0},
+        {1, 0, 0},
+        {0, 0, 0},
+    },
+    {
+        {0, 0, 0},
+        {0, 0, 1},
+        {0, 1, 0},
+        {0, 0, 0},
+        {1, 0, 0},
+        {0, 1, 0},
+        {0, 0, 0},
+    },
+};
 
+CpModel model = new CpModel();
+
+Literal[][][] shifts = new Literal[numNurses][numDays][numShifts];
+for (int n : allNurses) {
+  for (int d : allDays) {
+    for (int s : allShifts) {
+      shifts[n][d][s] = model.newBoolVar("shifts_n" + n + "d" + d + "s" + s);
+    }
+  }
+}
 
   }
 }
